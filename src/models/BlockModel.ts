2decaf76bd755a2a21b1d8a529e0db9c3f997373
heldermartins4 <eusouhelder123@gmail.com>
2024-10-05 17:38:23 -0300

    feat: block receive Json data

--- src/models/BlockModel.ts
+++ src/models/BlockModel.ts
@@ -1,9 +1,9 @@
 import { prisma } from "../database/prisma-client";
-import { ToolType } from "../types/tools";
+import { BlockType } from "../types/blockTypes";
 import PageModel from "./PageModel";
 
 interface IBlockData {
-    create(type:ToolType, pageId: string): Promise<any>
+    create(type:BlockType, pageId: string): Promise<any>
     getToolsByPageId(pageId: string): Promise<any>
 }
 
@@ -15,46 +15,9 @@ class BlockModel implements IBlockData {
         this.pgModel = new PageModel();
     }
 
-    async getNextIndex(pageId: string): Promise<number> {
-        const sequence = await prisma.block.findMany({
-            where: {
-                pageId
-            },
-            orderBy: {
-                orderY: "desc"
-            },
-            take: 1
-        });
+    // async getNextIndex(pageId: string): Promise<number> {}
 
-        return sequence.length ? sequence[0].orderY + 1 : 1;
-    }
-
-    async create(type:ToolType, pageId: string) {
-
-        const page = await this.pgModel.getPageById(pageId);
-
-        if (!page) throw new Error("Page not found");
-
-        const nextIndex = await this.getNextIndex(pageId);
-
-        const tool = await prisma.block.create({
-            data: {
-                type,
-                orderY: nextIndex,
-                orderX: 1,
-                data: JSON.stringify({
-                    ...(type === "text" && { text: "Some new Text" }),
-                }) as string,
-                page: {
-                    connect: {
-                        id: pageId
-                    }
-                }
-            }
-        });
-
-        return tool;
-    }
+    async create(type:BlockType, pageId: string) {}
 
     async getToolsByPageId(pageId: string) {
     
--- src/types/blockTypes.ts
+++ src/types/blockTypes.ts
@@ -1,4 +1,4 @@
-export type ToolType = 
+export type BlockType = 
     | "text"
     | "table"
     | "code"
