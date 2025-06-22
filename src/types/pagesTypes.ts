e7a5db4d4080cd32c754281c0bdc2ce1aa769bc7
@helder.santos0209 <helder.santos0209@gmail.com>
2025-06-22 02:02:30 -0300

    chore: update Prisma and @prisma/client to version 6.10.1; modify schema and types for consistency
    - Updated dependencies in package.json for Prisma and @prisma/client to version 6.10.1.
    - Adjusted migration_lock.toml for clarity in comments.
    - Enhanced schema.prisma by adding shadowDatabaseUrl and modifying formatting for better readability.
    - Added 'date' type to PagePropertyType in pagesTypes.ts and reformatted type definitions for consistency.

--- src/types/pagesTypes.ts
+++ src/types/pagesTypes.ts
@@ -1,51 +1,52 @@
 import { JsonValue } from "@prisma/client/runtime/library";
 
-export type PagePropertyType = 
-| "text"
-| "number"
-| "datetime"
-| "formula"
-| "selection"
-| "multi_selection"
-| "relation"
-| "rollup"
-| "assign"
-| "checkbox"
-| "status"
-| "button"
-| "calendar";
+export type PagePropertyType =
+  | "text"
+  | "number"
+  | "date"
+  | "datetime"
+  | "formula"
+  | "selection"
+  | "multi_selection"
+  | "relation"
+  | "rollup"
+  | "assign"
+  | "checkbox"
+  | "status"
+  | "button"
+  | "calendar";
 
 type Tags = {
-    name: string;
-    color: string;
+  name: string;
+  color: string;
 };
 
 export type Data = {
-    value?: string | number | boolean | null;
-    loadOrder: number;
-    icon?: string;
-    width?: number;
-    start?: string;
-    end?: string;
-    color?: string;
-    items?: Tags[];
-    tags?: Tags[];
+  value?: string | number | boolean | null;
+  loadOrder: number;
+  icon?: string;
+  width?: number;
+  start?: string;
+  end?: string;
+  color?: string;
+  items?: Tags[];
+  tags?: Tags[];
 } & JsonValue;
 
 export type PageProperty = {
-    id: string;
-    pageId: string;
-    title: string;
-    type: PagePropertyType;
-    data: Data;
-    trash: boolean | null;
-}
+  id: string;
+  pageId: string;
+  title: string;
+  type: PagePropertyType;
+  data: Data;
+  trash: boolean | null;
+};
 
 export type Page = {
-    id: string;
-    title: string;
-    ownerId: string;
-    trash: boolean | null;
-    
-    properties?: PageProperty[];
-}
+  id: string;
+  title: string;
+  ownerId: string;
+  trash: boolean | null;
+
+  properties?: PageProperty[];
+};
